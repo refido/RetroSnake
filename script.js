30c97c8e6ee7ed5c436ef99d2a916a74cb94b6ff
revido <revidoberliano@gmail.com>
2023-12-12 21:19:43 +0700

    Refactor game board and add snake drawing functionality

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
- console.log("Hello World");
+// Define html elements as variables for easy access in the script file
+const board = document.getElementById("game-board");
+
+// Define game variables
+let snake = [{ x: 11, y: 11 }]; // Snake starting position
+
+// Draw the game board, snake, and food
+function draw() {
+    board.innerHTML = ""; // Clear the board
+    drawSnake();
+}
+
+// Draw the snake on the board
+function drawSnake() {
+    snake.forEach((segment) => {
+        const snakeElement = createGameElement("div", "snake");
+        setPosition(snakeElement, segment);
+        board.appendChild(snakeElement);
+    });
+}
+
+// Create a snake or food
+function createGameElement(tag, className) {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+// Set the position of a snake or food
+function setPosition(element, position) {
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
+}
+
+// Test the draw function
+draw();
