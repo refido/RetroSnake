c57dbf0e4795ecc5be199bb27baa55f3f6fe3ba4
revido <revidoberliano@gmail.com>
2023-12-13 10:55:12 +0700

    Refactor snake movement and add initial direction

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // Define html elements as variables for easy access in the script file
-const board = document.getElementById("game-board");
+const board = document.getElementById('game-board');
 
 // Define game variables
 let gridSize = 20; // Size of the game board
 let snake = [{ x: 11, y: 11 }]; // Snake starting position
 let food = generateRandomPosition(); // Food starting position
+let direction = 'right'; // Snake starting direction
+// let direction = 'left'; 
+// let direction = 'up'; 
+// let direction = 'down'; 
 
 // Generate a random position for the food
 function generateRandomPosition() {
     const position = {
         x: Math.floor(Math.random() * gridSize) + 1, // Random number between 1 and 20
-        y: Math.floor(Math.random() * gridSize) + 1, 
+        y: Math.floor(Math.random() * gridSize) + 1,
     };
     return position;
 }
@@ -31,7 +35,7 @@ function setPosition(element, position) {
 // Draw the snake on the board
 function drawSnake() {
     snake.forEach((segment) => {
-        const snakeElement = createGameElement("div", "snake");
+        const snakeElement = createGameElement('div', 'snake');
         setPosition(snakeElement, segment);
         board.appendChild(snakeElement);
     });
@@ -39,17 +43,43 @@ function drawSnake() {
 
 // Draw the food on the board
 function drawFood() {
-    const foodElement = createGameElement("div", "food");
+    const foodElement = createGameElement('div', 'food');
     setPosition(foodElement, food);
     board.appendChild(foodElement);
 }
 
 // Draw the game board, snake, and food
 function draw() {
-    board.innerHTML = ""; // Clear the board
+    board.innerHTML = ''; // Clear the board
     drawSnake();
     drawFood();
 }
 
-// Test the draw function
-draw();
+// Move the snake
+function move() {
+    const head = snake[0]; // Get the head of the snake
+    switch (direction) {
+        case 'right':
+            head.x++; // Move the snake right by incrementing the x position, e.g. from (1, 1) to (2, 1)
+            break;
+        case 'left':
+            head.x--; // e.g. from (1, 1) to (0, 1)
+            break;
+        case 'up':
+            head.y--; // e.g. from (1, 1) to (1, 0)
+            break;
+        case 'down':
+            head.y++; // e.g. from (1, 1) to (1, 2)
+            break;
+        default:
+            break;
+    }
+    snake.unshift(head); // add the new head to make it look like it's moving
+    snake.pop(); // remove the snake in the last position to make it look like it's moving
+}
+
+// Test the function
+setInterval(() => {
+    move();
+    draw();
+}, 200);
